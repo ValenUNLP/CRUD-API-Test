826b047f2a86505ede9984667810e094752ffc91
Valentin Banegas <TheValenK@gmail.com>
2023-02-27 21:21:43 -0300

    Se añadio la funcionalidad: PUT

--- cypress/e2e/CRUD-API-TEST.cy.js
+++ cypress/e2e/CRUD-API-TEST.cy.js
@@ -1,12 +1,18 @@
 describe("CRUD API TEST",()=>{
 
     const endpoint = "http://localhost:3000/persons"
+
     const getElement = (endpoint)=>{
         return cy.request(endpoint)
     }
 
-    const addElements = (endpoint, element)=>{
+    const addElement = (endpoint, element)=>{
         cy.request("POST",endpoint, element)
+    }
+
+    const changeElement = (endpoint, element)=>{
+        const realEndpoint = `${endpoint}/${element.id}`
+        cy.request("PUT", realEndpoint, element)
     }
 
     it("Se debe validar la cantidad de elementos del endpoint", ()=>{
@@ -16,20 +22,35 @@ describe("CRUD API TEST",()=>{
         })
     })
 
-    it.only("Se deberia añadir un elemento",()=>{
+    it("Se deberia añadir un elemento",()=>{
 
         const newElement = {
+            id: 3,
+            name: "Valentin4",
+            surname: "Banegas4"
+        }
+
+        addElement(endpoint, newElement)
+
+        getElement(endpoint).its("body").should(body =>{
+            expect(body).to.have.length(3)
+            expect(body[newElement.id - 1]).deep.eq(newElement)
+        })
+    })
+
+    it("Se deberia cambiar un elemento", ()=>{
+
+        const fixedElement = {
             id: 3,
             name: "Valentin3",
             surname: "Banegas3"
         }
 
-        addElements(endpoint, newElement)
+        changeElement(endpoint, fixedElement)
 
-        getElement(`${endpoint}/${newElement.id}`).its("body").should(body  =>{
-            console.log(body)
-            expect(body).deep.eq(newElement)
+        getElement(endpoint).its("body").should(body =>{
+            expect(body).to.have.length(3)
+            expect(body[fixedElement.id - 1]).deep.eq(fixedElement)
         })
-
     })
 })
