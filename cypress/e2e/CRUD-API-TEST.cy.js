a9da84fd719065675644f72a39e502e90e0a975f
Valentin Banegas <TheValenK@gmail.com>
2023-02-27 21:09:13 -0300

    Se añadio la funcionalidad de: POST

--- cypress/e2e/CRUD-API-TEST.cy.js
+++ cypress/e2e/CRUD-API-TEST.cy.js
@@ -1,9 +1,12 @@
 describe("CRUD API TEST",()=>{
 
     const endpoint = "http://localhost:3000/persons"
-
     const getElement = (endpoint)=>{
         return cy.request(endpoint)
+    }
+
+    const addElements = (endpoint, element)=>{
+        cy.request("POST",endpoint, element)
     }
 
     it("Se debe validar la cantidad de elementos del endpoint", ()=>{
@@ -11,5 +14,22 @@ describe("CRUD API TEST",()=>{
         getElement(endpoint).its("body").should(body  =>{
             expect(body).to.have.length(2)
         })
+    })
+
+    it.only("Se deberia añadir un elemento",()=>{
+
+        const newElement = {
+            id: 3,
+            name: "Valentin3",
+            surname: "Banegas3"
+        }
+
+        addElements(endpoint, newElement)
+
+        getElement(`${endpoint}/${newElement.id}`).its("body").should(body  =>{
+            console.log(body)
+            expect(body).deep.eq(newElement)
+        })
+
     })
 })
